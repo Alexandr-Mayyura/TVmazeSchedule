edea83354524fcf60333c7b52a95e718748b3106
Alexandr-Mayyura <sonickill@yandex.ru>
2022-11-05 14:26:16 +0300

    add collectionView

---
+++ TVmazeSchedule/EpisodeCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  EpisodCollectionViewCell.swift
+//  TVmazeSchedule
+//
+//  Created by Aleksandr Mayyura on 04.11.2022.
+//
+
+import UIKit
+
+class EpisodeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var nameEpisodLabel: UILabel!
+    
+    @IBOutlet var nameShowLabel: UILabel!
+    @IBOutlet var showImageView: UIImageView!
+    
+
+    
+}
--- TVmazeSchedule/ViewControllers/EpisodeInfoViewController.swift
+++ TVmazeSchedule/ViewControllers/EpisodeInfoViewController.swift
@@ -12,10 +12,31 @@ enum Link: String {
 }
 
 class EpisodeInfoViewController: UIViewController {
-
+    
+    @IBOutlet var episodeCollectionView: UICollectionView!
+    
+    private var episodeInfo: [EpisodeInfo] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       fetchEpisodeInfo()
+        fetchEpisodeInfo()
+        episodeCollectionView.dataSource = self
+    }
+}
+
+extension EpisodeInfoViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+       return episodeInfo.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "episodCell", for: indexPath)
+        guard let cell = cell as? EpisodeCollectionViewCell else { return UICollectionViewCell() }
+  
+        let aaa = self.episodeInfo[indexPath.item].show
+        cell.nameShowLabel.text = aaa?.name
+        cell.nameEpisodLabel.text = episodeInfo[indexPath.item].name
+        return cell
     }
 }
 
@@ -24,19 +45,23 @@ extension EpisodeInfoViewController {
     private func fetchEpisodeInfo() {
         guard let url = URL(string: Link.scheduleURL.rawValue) else { return }
         
-        URLSession.shared.dataTask(with: url) { data, _, error in
+        URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
             guard let data = data else {
                 print(error?.localizedDescription ?? "No error description")
                 return
             }
-            
             let decoder = JSONDecoder()
             do {
                 let scheduleEpisodes = try decoder.decode(
                     [EpisodeInfo].self,
                     from: data
                 )
-                print(scheduleEpisodes)
+                    self?.episodeInfo = scheduleEpisodes
+                
+                DispatchQueue.main.async {
+                    self?.episodeCollectionView.reloadData()
+                   
+                }
             } catch let error {
                 print(error.localizedDescription)
             }
